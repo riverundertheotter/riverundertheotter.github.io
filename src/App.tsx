7629ae99e5091e45ada97c7e6c14a84ca51db6ad
River Cook <rivercook2602@gmail.com>
2025-04-15 12:10:42 -0500

    Adding images, playing with environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import buizel from './assets/buizel.jpeg';
+
 import './App.css'
 
 function App() {
@@ -11,8 +13,8 @@ function App() {
         <div><a href='https://github.com/riverundertheotter'>Github </a></div>
         <div><a href='https://www.linkedin.com/in/river-cook/'>LinkedIn </a></div>
       </div>
+      <img className='Buizel' src={buizel} alt='Buizel' />
     </div>
-
   )
 }
 
