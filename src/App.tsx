a95f20fc35e54b92c4cef09b097b588df50e8663
River Cook <rivercook2602@gmail.com>
2025-04-15 11:23:27 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='Header'>
+        Welcome to River Land!
+      </div>
+    </div>
+
+  )
+}
+
+export default App
